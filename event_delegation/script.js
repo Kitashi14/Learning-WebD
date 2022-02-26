b98ab8bf8ea45e29d2a70ec5b26cb1de43e20dda
Kitashi <rishavrajaryan14@gmail.com>
2022-02-27 00:56:12 +0530

    event delegation example complete

--- event_delegation/script.js
+++ event_delegation/script.js
@@ -1,9 +1,25 @@
 const category = document.querySelector("#category");
 
-category.addEventListener("click", (e)=>{
-    if(e.target.id != "category"){
-        window.location.href = "/" + e.target.id;
-    }
+// T-1 ------------start
+
+// category.addEventListener("click", (e) => {
+//   console.log(e.target);
+//   if (e.target.id != "category") {
+//     window.location.href = "/" + e.target.id;
+//   }
+// });
+
+//T-1 ----------------end
+
+// T-2 ------------start
+
+category.addEventListener("click", (e) => {
+    console.log(e);
+  console.log(e.target);
+  if (e.target.tagName == "LI") {
+    window.location.href = "/" + e.target.id;
+  }
 });
+//T-2 ----------------end
 
 //here instead of writing events for multiple items we write for a single common parent element and use the concept of evneny delegation.
