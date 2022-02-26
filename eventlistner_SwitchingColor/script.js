e599e13b19a2ad65ffef4b1418ea0f386871c470
Kitashi <rishavrajaryan14@gmail.com>
2022-02-26 21:24:45 +0530

    event bubbling and capturing example code added

--- eventlistner_SwitchingColor/script.js
+++ eventlistner_SwitchingColor/script.js
@@ -8,15 +8,18 @@ const middleBoxBg = getComputedStyle(middleBox).backgroundImage;
 console.log("color of middle box is : " + middleBoxBg);
 
 //method addEventListner take 2 parameter ( type of event , callback func)
+
+//T-1 --------------------start
+
 outerBox.addEventListener("click", (e) => {
-  console.log(e.target);
+  console.log(e.target);  // "target" mostly used property
   const bodyBg = getComputedStyle(body).backgroundImage;
   const target_color = getComputedStyle(e.target).backgroundImage;
   const temp = target_color;
   e.target.style.backgroundImage = bodyBg;
   body.style.backgroundImage = temp;
-  e.stopPropagation();
-  // "target" mostly used property
+  e.stopPropagation();  //stoping further bubble or capture propagation
+
 });
 
 middleBox.addEventListener("click", (e) => {
@@ -36,8 +39,64 @@ innerBox.addEventListener("click", (e) => {
   const temp = target_color;
   e.target.style.backgroundImage = bodyBg;
   body.style.backgroundImage = temp;
-  e.stopPropagation(); //stoping further bubble or capture propagation
+  e.stopPropagation();
 });
+
+//T-1 -----------------------end
+
+//Event bubbling example
+
+//T-2 -------------------------start
+
+// outerBox.addEventListener("click", (e) => {
+//   console.log("Outerbox (grandparent) clicked");
+// });
+
+// middleBox.addEventListener("click", (e) => {
+//   console.log("Middlebox (parent) clicked");
+// });
+
+// innerBox.addEventListener("click", (e) => {
+//   console.log("Innerbox (child) clicked");
+// });
+
+//T-2 -------------------------end
+
+//Event capturing example
+
+//T-3 -------------------------start
+
+// outerBox.addEventListener("click", (e) => {
+//   console.log("Outerbox (grandparent) clicked");
+// }, true);
+
+// middleBox.addEventListener("click", (e) => {
+//   console.log("Middlebox (parent) clicked");
+// }, true);
+
+// innerBox.addEventListener("click", (e) => {
+//   console.log("Innerbox (child) clicked");
+// }, true);
+
+//T-3 -------------------------end
+
+//Event capturing example (not all set to capture)
+
+//T-4 -------------------------start
+
+// outerBox.addEventListener("click", (e) => {
+//   console.log("Outerbox (grandparent) clicked");
+// }, );
+
+// middleBox.addEventListener("click", (e) => {
+//   console.log("Middlebox (parent) clicked");
+// }, true);
+
+// innerBox.addEventListener("click", (e) => {
+//   console.log("Innerbox (child) clicked");
+// }, true);
+
+//T-4 -------------------------end
 
 //another way of adding event listener to each element
 
