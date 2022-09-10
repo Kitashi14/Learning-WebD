f7b5e8e6bc295706888211ae020fa9ba2d587682
Kitashi <rishavrajaryan14@gmail.com>
2022-09-10 20:36:53 +0530

    arranged demo server dir

--- Demo server/Server 1/server.js
+++ Demo server/Server 1/server.js
@@ -3,24 +3,24 @@ const app = express();
 const port = 4002;
 
 //adding a middleware for setting headers sent api requests from differnt origin for allowing its execution (for tackling CORS);
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader(
-//       "Access-Control-Allow-Headers",
-//       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//     );
-//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
-//     next();
-//   });
+    next();
+  });
 
 var count = 0;
 app.get("/apiCall", (req, res) => {
-  const response = { hello: "brother" };
+  const response = {response : "hello brother"};
 
   count++;
-  console.log("Got a request " + count);
-  console.log({ response: response });
+  console.log("Got request " + count);
+  console.log(response);  
   console.log("\n\n");
   res.send(response);
 });
